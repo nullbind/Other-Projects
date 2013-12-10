036349474363f5ed02bbdf5f5d3e7ad2590f7682
nullbind <scott.sutherland@nullbind.com>
2013-12-09 21:45:37 -0600

    updates, couple bugs left

---
+++ PowerShellery/Get-SPN.ps1
@@ -0,0 +1,254 @@
+﻿
+##########################################
+# ServicePrincipalName Query Function
+##########################################
+function Get-SPN
+{
+
+<#
+	.SYNOPSIS
+    Displays Service Principal Names (SPN) for domain accounts based on SPN, domain account, 
+    or domain group for the current domain via LDAP queries.
+
+	.DESCRIPTION
+    Displays Service Principal Names (SPN) for domain accounts based on SPN, domain account, 
+    or domain group for the current domain via LDAP queries.  This information can be used to 
+    identify systems running specific services, and in some cases locate systems that specific 
+    domain accounts are running on.  For example, it could be used to locate systems where a 
+    specific domain account was used to run SQL Server.  It can also be used to identify systems 
+    where Domain Admins or other domain group members may be logged in.  Which can be valuable 
+    when escalating privileges during penetration tests.
+	
+	.EXAMPLE	 
+    Get-SPN -type service -search www"
+
+	.EXAMPLE	 
+    Get-SPN -type user -search "sqladmin" 
+
+	.EXAMPLE	 
+    Get-SPN -type group -search "Domain Admins" 
+
+	.EXAMPLE	 
+    Get-SPN -type group -search "Domain Admins" -list yes
+
+	.EXAMPLE	 
+    Get-SPN -type group -search "Domain Admins" -list yes | Select Server
+	
+    .LINK
+    http://www.netspi.com
+    http://msdn.microsoft.com/en-us/library/windows/desktop/ms677949(v=vs.85).aspx
+    http://technet.microsoft.com/en-us/library/cc731241.aspx
+    http://technet.microsoft.com/en-us/library/cc978021.aspx
+
+	.NOTES
+    Scott Sutherland 2013, NetSPI
+    The LDAP function skeleton was taken from Carlos Perez's "Get-AuditDSDisabledUserAcount" function.
+	#>	
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory=$false,
+        HelpMessage="Credentials to use when connecting to a Domain Controller.")]
+        [System.Management.Automation.PSCredential]
+        [System.Management.Automation.Credential()]$Credential = [System.Management.Automation.PSCredential]::Empty,
+        
+        [Parameter(Mandatory=$false,
+        HelpMessage="Domain controller for Domain and Site that you want to query against.")]
+        [string]$DomainController,
+
+        [Parameter(Mandatory=$false,
+        HelpMessage="Maximum number of Objects to pull from AD, limit is 1,000 .")]
+        [int]$Limit = 1000,
+
+        [Parameter(Mandatory=$false,
+        HelpMessage="scope of a search as either a base, one-level, or subtree search, default is subtree.")]
+        [ValidateSet("Subtree","OneLevel","Base")]
+        [string]$SearchScope = "Subtree",
+
+        [Parameter(Mandatory=$false,
+        HelpMessage="Distinguished Name Path to limit search to.")]
+        [string]$SearchDN,
+
+        [Parameter(Mandatory=$True,
+        HelpMessage="Set type user, group, or service to search by.")]
+	    [string]$Type,
+
+		[Parameter(Mandatory=$True,
+        HelpMessage="Define search for user, group, or SPN service name. Wildcards are accepted")]
+	    [string]$Search,
+
+        [Parameter(Mandatory=$false,
+        HelpMessage="View minimal information that includes the accounts,affected systems,and registered services.  Nice for getting quick list of DAs.")]
+	    [string]$List
+    )
+
+    Begin
+    {
+        if ($DomainController -and $Credential.GetNetworkCredential().Password)
+        {
+            $objDomain = New-Object System.DirectoryServices.DirectoryEntry "LDAP://$($DomainController)", $Credential.UserName,$Credential.GetNetworkCredential().Password
+            $objSearcher = New-Object System.DirectoryServices.DirectorySearcher $objDomain
+        }
+        else
+        {
+            $objDomain = [ADSI]""  
+            $objSearcher = New-Object System.DirectoryServices.DirectorySearcher $objDomain
+        }
+    }
+
+    Process
+    {
+
+        # Format the current domain for LDAP - SPS
+        $current_domain = $env:USERDNSDOMAIN
+        $domain_list = ""
+        $current_domain.split(".")| foreach { $domain_list = $domain_list + ",DC=$_" }
+	
+	    # Create query options - SPS
+	    $QueryGroup = "(&(objectCategory=user)(memberOf=CN=$Search,CN=Users$domain_list))"	
+	    $QueryUser = "(samaccountname=$Search)"
+	    $QuerySpn = "(ServicePrincipalName=$Search)"
+        
+        # Check SPN query type - SPS
+	    if(($Type -eq "group") -or ($Type -eq "user") -or ($Type -eq "spn")){
+		
+		    # Define query based on type
+		    switch ($Type) 
+		    { 
+			    "group" {$MyFilter = $QueryGroup} 
+			    "user" {$MyFilter = $QueryUser} 
+			    "spn" {$MyFilter = $QuerySpn} 
+			    default {"Invalid query type."}
+		    }
+        }
+		
+        $ObjSearcher.PageSize = $Limit
+        $ObjSearcher.Filter = $Myfilter
+        $ObjSearcher.SearchScope = $SearchScope
+
+        if ($SearchDN)
+        {
+            $objSearcher.SearchDN = New-Object System.DirectoryServices.DirectoryEntry("LDAP://$($SearchDN)")
+        }
+
+        # Get record count
+        $records = $objSearcher.FindAll()
+		$record_count = $records.count
+
+        # Display search results if results exist
+        if ($record_count -gt 0){
+                
+            # Create data table to house data
+            $dataTable = New-Object System.Data.DataTable 
+
+            # Create and name column in table
+            $dataTable.Columns.Add("Account") | Out-Null
+            $dataTable.Columns.Add("Server") | Out-Null
+            $dataTable.Columns.Add("Service") | Out-Null            
+
+            # Display records                
+            $ObjSearcher.FindAll() | ForEach-Object {
+
+                # Fill hash array with results                    
+                $UserProps = [ordered]@{}                    
+                $UserProps.Add('Name', "$($_.properties.name)")
+                $UserProps.Add('SAMAccount', "$($_.properties.samaccountname)")
+                $UserProps.Add('Description', "$($_.properties.description)")
+                $UserProps.Add('UserPrincipal', "$($_.properties.userprincipalname)")
+                $UserProps.Add('DN', "$($_.properties.distinguishedname)")
+                $UserProps.Add('Created', [dateTime]"$($_.properties.whencreated)")
+                $UserProps.Add('LastModified', [dateTime]"$($_.properties.whenchanged)")
+                $UserProps.Add('PasswordLastSet', [dateTime]::FromFileTime("$($_.properties.pwdlastset)"))                    
+                $UserProps.Add('AccountExpires',( &{$exval = "$($_.properties.accountexpires)"
+                If (($exval -eq 0) -or ($exval -gt [DateTime]::MaxValue.Ticks))
+                {
+                    $AcctExpires = "<Never>"
+                }Else{
+                    $Date = [DateTime]$exval
+                    $AcctExpires = $Date.AddYears(1600).ToLocalTime()
+                }
+
+               $AcctExpires
+            
+                    }))
+                    $UserProps.Add('LastLogon', [dateTime]::FromFileTime("$($_.properties.lastlogon)"))
+                    $UserProps.Add('GroupMembership', "$($_.properties.memberof)")
+                    $UserProps.Add('SPN Count', "$($_.properties['ServicePrincipalName'].count)") 
+                    #$UserProps.Add('ServicePrincipalName', "$($_.properties['ServicePrincipalName'])")                  
+
+                    # Only display line for detailed view
+                    If (!$list){
+
+                        # Format array as object and display records
+                        [pscustomobject]$UserProps 
+                    }
+
+                    $SPN_Count = $_.properties['ServicePrincipalName'].count
+
+                    if ($SPN_Count -gt 0)
+				    {
+                        
+                        # Only display line for detailed view
+                        If (!$list){
+					        Write-Output "ServicePrincipalNames (SPN):"
+					        $_.properties['ServicePrincipalName']
+                        }
+                        
+                        # Add records to data table
+                        foreach ($item in $_.properties['ServicePrincipalName'])
+                        {
+                            $x =  $_.properties['ServicePrincipalName'].split("/")[1].split(":")[0]	
+                            $y =  $_.properties['ServicePrincipalName'].split("/")[0]                                                                                   
+                            $dataTable.Rows.Add($($_.properties.samaccountname), $x, $y) | Out-Null  
+                        }
+
+				    }            
+                    
+                    
+                    # Only display line for detailed view
+                    If (!$list){
+                        Write-Host "-------------------------------------------------------------"
+                    }
+
+                } 
+
+                    # Only display lines for detailed view
+                    If (!$list){
+
+                        # Display number of accounts found
+			            Write-Host "Found $record_count accounts that matched your search."   
+                        Write-Host "-------------------------------------------------------------"                                    
+
+                        # Dispaly list view of results
+                        $dataTable | Sort-Object Account,Server,Service | Format-Table -AutoSize
+
+                        # Display number of service instances
+                        $instance_count = $dataTable.rows.count
+			            Write-Host "-------------------------------------------------------------"
+                        Write-Host "Found $instance_count service instances that matched your search."
+                        Write-Host "-------------------------------------------------------------"
+                    }else{
+                        
+                        # Dispaly list view of results
+                        $dataTable | Sort-Object Account,Server,Service 
+                    }
+
+        }else{
+            
+            # Display fail
+			Write-Host " " 
+			Write-Host "No records were found that match your search."
+			Write-Host ""
+        }
+
+        
+    }
+}
+
+
+#Buggy
+#Get-SPN -type service -search "www"
+
+#Done
+#Get-SPN -type user -search "sqladmin"
+#Get-SPN -type group -search "Domain Admins"
+#Get-SPN -type group -search "Domain Admins" -list yes
