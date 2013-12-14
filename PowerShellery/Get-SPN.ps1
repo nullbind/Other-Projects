ae11124fc8d297e7b7f804f15665439824273bb5
nullbind <scott.sutherland@nullbind.com>
2013-12-13 23:11:28 -0600

    more tests, more fixes

--- PowerShellery/Get-SPN.ps1
+++ PowerShellery/Get-SPN.ps1
@@ -7,17 +7,18 @@ function Get-SPN
 
 	<#
 	.SYNOPSIS
-	Displays Service Principal Names (SPN) for domain accounts based on SPN, domain account, 
-	or domain group for the current domain via LDAP queries.
+	Displays Service Principal Names (SPN) for domain accounts based on SPN service name, 
+	domain account, or domain group via LDAP queries.
 
 	.DESCRIPTION
-	Displays Service Principal Names (SPN) for domain accounts based on SPN, domain account, 
-	or domain group for the current domain via LDAP queries.  This information can be used to 
-	identify systems running specific services, and in some cases locate systems that specific 
-	domain accounts are running on.  For example, it could be used to locate systems where a 
-	specific domain account was used to run SQL Server.  It can also be used to identify systems 
-	where Domain Admins or other domain group members may be logged in.  Which can be valuable 
-	when escalating privileges during penetration tests.
+	Displays Service Principal Names (SPN) for domain accounts based on SPN service name, 
+	domain account, or domain group via LDAP queries. This information can be used to 
+	identify systems running specific services and the domain accounts running them.  
+	For example, this script could be used to locate systems where a specific domain 
+	account was configured to run SQL Server.  It can also be used to help find systems
+	where members of the Domain Admins group might be logged in if the accounts where 
+	used to run services on the domain (which is very common).  So this should be handy
+	for both system administrators and penetration testers.
 	
 	.EXAMPLE	 
 	Get-SPN -type service -search "MSSQLSvc*"
